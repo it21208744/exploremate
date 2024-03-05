4bcc5ee927d6c21798fd3e4a026b07781de8e096
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-03-06 00:32:36 +0530

    addedTravelersRoutes

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ import express from 'express'
 const app = express()
 
 import taxiRouter from './routes/taxiRouter.js'
-
+import travelersRouter from './routes/travelersRouter.js'
 // app.use('api/v1/auth')
-
-// app.use("api/v1/travelers", )
 
 // app.use('api/v1/hotelowner')
 app.use(express.json())
+
+app.use('/api/v1/travelers', travelersRouter)
 
 app.use('/api/v1/taxi', taxiRouter)
 
