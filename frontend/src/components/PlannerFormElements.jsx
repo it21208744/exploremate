695e0c0ea4ec2f6158cb5a6dcbb306a2d83c1c38
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-03-10 15:56:58 +0530

    add functionality to take the input from the planner form :)

--- frontend/src/components/PlannerFormElements.jsx
+++ frontend/src/components/PlannerFormElements.jsx
@@ -1,14 +1,24 @@
+import { Form } from 'react-router-dom'
+
+export const action = async ({ request }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+  console.log(data)
+  return null
+}
+
 const PlannerFormElements = () => {
   return (
     <div>
       <h1>Plan your next adventure</h1>
-      <form className="form">
+      <Form method="post" className="form">
         <div>
           <label htmlFor="location" className="form-label">
             Where to go?
           </label>
           <input
             type="text"
+            name="location"
             // name="location"
             className="formInput"
             id="location"
@@ -20,6 +30,7 @@ const PlannerFormElements = () => {
           </label>
           <input
             type="date"
+            name="date"
             // name="date"
             className="formInput-date"
             id="date"
@@ -31,6 +42,7 @@ const PlannerFormElements = () => {
           </label>
           <input
             type="days"
+            name="days"
             // name="days"
             className="formInput"
             id="days"
@@ -38,7 +50,7 @@ const PlannerFormElements = () => {
         </div>
 
         <button type="submit">Lets travel</button>
-      </form>
+      </Form>
     </div>
   )
 }
