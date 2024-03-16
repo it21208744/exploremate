163b934625e04188f479b4e0eaad8b97aa6e11a3
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-03-16 21:01:04 +0530

    removed unnecessary components and add some useful comments that i will use in future

--- controllers/travelersController.js
+++ controllers/travelersController.js
@@ -4,10 +4,10 @@ import { checkWeather } from '../components/weather.js'
 
 export const addPlan = async (req, res) => {
   const { location, days, type } = req.body
-  const plans = await generatePlans(location, days, type)
-  const plansContent = jsonStringToObject(plans.content)
+  const plansContent = await generatePlans(location, days, type)
+  // const plansContent = jsonStringToObject(plans.content)
   const weather = await checkWeather(location)
-
+  // console.log(weather.wind.speed)
   res.json({ plansContent, weather })
 }
 
@@ -22,3 +22,9 @@ export const getSinglePlan = (req, res) => {
 export const deletePlan = (req, res) => {
   res.send(`delete a plan`)
 }
+
+//const description = weather.weather[0].main
+//const descSituation = weather.weather[0].description
+
+//const temp = weather.main
+//const windSpeed = weather.wind.speed
