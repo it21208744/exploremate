3997bb301209223e867a7521a3c423b423e9e586
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-03-16 21:03:48 +0530

    idk

---
+++ frontend/src/components/PlannerOutputGuide.jsx
@@ -0,0 +1,38 @@
+const PlannerOutput = (planAndWeather) => {
+  const plans = planAndWeather?.planAndWeather?.plansContent
+  let plansAsArray = []
+
+  if (plans) {
+    plansAsArray = Object.values(plans).map((item) => item.actions)
+  }
+
+  return (
+    <div>
+      <h2>Need a guide?</h2>
+
+      <ul>
+        {plansAsArray.map((plan, index) => {
+          return (
+            <li key={index}>
+              Day {index + 1} - {plan}
+            </li>
+          )
+        })}
+      </ul>
+
+      <h2>You might need these things...</h2>
+
+      <ul>
+        <li>Cap</li>
+        <li>Cap</li>
+        <li>Cap</li>
+        <li>Cap</li>
+        <li>Cap</li>
+        <li>Cap</li>
+        <li>Cap</li>
+        <li>Cap</li>
+      </ul>
+    </div>
+  )
+}
+export default PlannerOutput
