ef98a836def92ee0d016294ab215c154f1d53b9f
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-03-16 21:05:12 +0530

    removed some unnecessary elements

--- frontend/src/pages/travelers/Planner.jsx
+++ frontend/src/pages/travelers/Planner.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom'
-import HorizontalBar from '../../components/navBars/travelersComp/HorizontalBar'
 import Wrapper from '../../assets/wrappers/travelersWrappers/Planner'
 import PlannerFormElements from '../../components/PlannerFormElements'
 
@@ -7,9 +6,7 @@ const Planner = () => {
   return (
     <Wrapper>
       <div className="plannerContainer">
-        <HorizontalBar />
         <PlannerFormElements />
-
         <Outlet />
       </div>
     </Wrapper>
