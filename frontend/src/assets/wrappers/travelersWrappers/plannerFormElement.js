205f367ab5c6ba2aedab7eb8d0768f3ab5c842ef
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-03-18 10:13:08 +0530

    some css added

---
+++ frontend/src/assets/wrappers/travelersWrappers/plannerFormElement.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  .pageTitle {
+    position: relative;
+    top: -16vw;
+    left: 25vw;
+  }
+
+  .form {
+    position: relative;
+    top: -14vw;
+    /* left: 25vw; */
+  }
+
+  .defaultContent {
+    position: absolute;
+    /* left: 25vw; */
+    width: 100%;
+    height: 100vw;
+    background: wheat;
+  }
+`
+
+export default Wrapper
--- frontend/src/components/PlannerFormElements.jsx
+++ frontend/src/components/PlannerFormElements.jsx
@@ -4,6 +4,7 @@ import PlannerOutputGuide from './PlannerOutputGuide'
 import { useState, useEffect } from 'react'
 import Travelpedia from '../pages/travelers/Travelpedia'
 import { toast } from 'react-toastify'
+import Wrapper from '../assets/wrappers/travelersWrappers/plannerFormElement'
 
 export const action = async ({ request }) => {
   const formData = await request.formData()
@@ -36,78 +37,87 @@ const PlannerFormElements = () => {
   }, [weather])
 
   return (
-    <div>
-      <h1>Plan your next adventure</h1>
-      <Form method="post" className="form">
-        <div>
-          <label htmlFor="location" className="form-label">
-            Where to go?
-          </label>
-          <input
-            type="text"
-            name="location"
-            className="formInput"
-            id="location"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="date" className="form-label">
-            When?
-          </label>
-          <input type="date" name="date" className="formInput-date" id="date" />
-        </div>
-        <div>
-          <label htmlFor="days" className="form-label">
-            How many days?
-          </label>
-          <input
-            type="days"
-            name="days"
-            className="formInput"
-            id="days"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="type" className="form-label">
-            Type?
-          </label>
-          <input type="text" name="type" className="formInput" id="type" />
-        </div>
+    <Wrapper>
+      <div>
+        <h1 className="pageTitle">Plan your next adventure</h1>
+        <Form method="post" className="form">
+          <div>
+            <label htmlFor="location" className="form-label">
+              Where to go?
+            </label>
+            <input
+              type="text"
+              name="location"
+              className="formInput"
+              id="location"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="date" className="form-label">
+              When?
+            </label>
+            <input
+              type="date"
+              name="date"
+              className="formInput-date"
+              id="date"
+            />
+          </div>
+          <div>
+            <label htmlFor="days" className="form-label">
+              How many days?
+            </label>
+            <input
+              type="days"
+              name="days"
+              className="formInput"
+              id="days"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="type" className="form-label">
+              Type?
+            </label>
+            <input type="text" name="type" className="formInput" id="type" />
+          </div>
 
-        <button type="submit" disabled={isSubmitting}>
-          {isSubmitting ? 'Creating a plan...' : 'Lets travel'}
-        </button>
-      </Form>
-      {/* {outputSituation === 'default' ? <h1>default</h1> : null} */}
-      {outputSituation === 'outPutPresents' ? (
-        <ul>
-          <li>
-            <button type="button" onClick={() => changeGuide(true)}>
-              GUIDE
-            </button>
-          </li>
-          <li>
-            <button type="button" onClick={() => changeGuide(false)}>
-              TRAVELPEDIA
-            </button>
-          </li>
-        </ul>
-      ) : null}
-      {outputSituation === 'outPutPresents' ? (
-        isGuide ? (
-          <PlannerOutputGuide
-            planAndWeather={planAndWeather}
-            packingList={packingList}
-          />
+          <button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? 'Creating a plan...' : 'Lets travel'}
+          </button>
+        </Form>
+        {/* {outputSituation === 'default' ? <h1>default</h1> : null} */}
+        {outputSituation === 'outPutPresents' ? (
+          <ul>
+            <li>
+              <button type="button" onClick={() => changeGuide(true)}>
+                GUIDE
+              </button>
+            </li>
+            <li>
+              <button type="button" onClick={() => changeGuide(false)}>
+                TRAVELPEDIA
+              </button>
+            </li>
+          </ul>
+        ) : null}
+        {outputSituation === 'outPutPresents' ? (
+          isGuide ? (
+            <PlannerOutputGuide
+              planAndWeather={planAndWeather}
+              packingList={packingList}
+            />
+          ) : (
+            <Travelpedia location={location} weather={weather} />
+          )
         ) : (
-          <Travelpedia location={location} weather={weather} />
-        )
-      ) : (
-        <h1>Here goes some bullshit</h1>
-      )}
-    </div>
+          <div className="defaultContent">
+            <h1>Here goes some bullshit</h1>
+          </div>
+        )}
+      </div>
+    </Wrapper>
   )
 }
 export default PlannerFormElements
