dde28648cb71a490df262ce8ca7ae1c46969df92
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-03-25 12:12:26 +0530

    single Plans

--- frontend/src/components/SinglePlanInfo.jsx
+++ frontend/src/components/SinglePlanInfo.jsx
@@ -1,4 +1,8 @@
 const SinglePlanInfo = () => {
-  return <div>SinglePlanInfo</div>
+  return (
+    <div>
+      <h1>HE he this is a single plan</h1>
+    </div>
+  )
 }
 export default SinglePlanInfo
