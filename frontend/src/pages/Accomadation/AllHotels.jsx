b67ef7d0a1fa57399e4d2ceccfa30602431d80dc
IT21226182 <IT21226182@my.sliit.lk>
2024-03-30 12:39:36 +0530

    delete confirmation try

--- frontend/src/pages/Accomadation/AllHotels.jsx
+++ frontend/src/pages/Accomadation/AllHotels.jsx
@@ -49,106 +49,89 @@ export default function AllSales() {
         localStorage.setItem("PackDetail", PackDetail);
     };
 
- //heading
-const lableStyle1 = { 
-    color:"#042630" ,
-    //fontWeight: "300",  
-    fontSize: "20px", 
-   // marginBottom: "1000px"
-  };
+    // Styles
+    const lableStyle1 = { 
+        color:"#042630" ,
+        fontSize: "20px", 
+    };
 
+    const buttonDelete = {
+        display: "inline-block",
+        backgroundColor: "#D71515",
+        color:"#fff",
+        textTransform:"uppercase",
+        letterSpacing:"1px",
+        fontSize: "14px",
+        width:"90px",
+        height:"36px",
+        border:"none",
+        borderRadius: "20px",
+        cursor: "pointer",
+    };
 
-  const buttonDelete = {
-    display: "inline-block",
-    //backgroundImage: "linear-gradient(125deg,#042630,#4c7273)",
-    backgroundColor: "#D71515",
-    color:"#fff",
-    textTransform:"uppercase",
-    letterSpacing:"1px",
-    fontSize: "14px",
-      width:"90px",
-      height:"36px",
-      border:"none",
-      borderRadius: "20px",
-      cursor: "pointer",
-      
-  };
+    const buttonEdit = {
+        display: "inline-block",
+        backgroundColor: "green",
+        color:"#fff",
+        textTransform:"uppercase",
+        letterSpacing:"1px",
+        fontSize: "14px",
+        width:"90px",
+        height:"36px",
+        border:"none",
+        borderRadius: "20px",
+        cursor: "pointer",
+    };
 
-  const buttonEdit = {
-    display: "inline-block",
-    //backgroundImage: "linear-gradient(125deg,#042630,#4c7273)",
-    backgroundColor: "green",
-    color:"#fff",
-    textTransform:"uppercase",
-    letterSpacing:"1px",
-    fontSize: "14px",
-      width:"90px",
-      height:"36px",
-      border:"none",
-      borderRadius: "20px",
-      cursor: "pointer",
-      
-  };
+    const cardstyle ={
+        overflow : "hidden",
+        boxShadow:"0 2px 20px ",
+        borderRadius: "$radius",
+        transition: "transform 200ms ease-in",
+        padding:"30px",
+        backdropFilter: "blur(5px)",
+        background: "linear-gradient(rgba(255, 255, 255, 0.7),rgba(255, 255, 255, 0.3))",
+        width: "1200px",
+        marginLeft:"180px",
+    };
+    
+    const tableStyle = {
+        borderCollapse: 'collapse',
+        width: '100%',
+        color: '#4c7273'
+    };
 
+    const thStyle = {
+        border: '1px solid #ddd',
+        padding: '8px',
+        textAlign: 'left',
+        backgroundColor: '#f2f2f2'
+    };
 
-  const cardstyle ={
-    overflow : "hidden",
-    boxShadow:"0 2px 20px ",
-    borderRadius: "$radius",
-    transition: "transform 200ms ease-in",
-    padding:"30px",
-    backdropFilter: "blur(5px)",
-    background: "linear-gradient(rgba(255, 255, 255, 0.7),rgba(255, 255, 255, 0.3))",
-    width: "1100px",
-    marginLeft:"180px",
-   
-  };
-  
-
-  const tableStyle = {
-    borderCollapse: 'collapse',
-    width: '100%',
-    color: '#4c7273'
-  };
-
-  const thStyle = {
-    border: '1px solid #ddd',
-    padding: '8px',
-    textAlign: 'left',
-    backgroundColor: '#f2f2f2'
-  };
-
-  const tdStyle = {
-    border: '1px solid #ddd',
-    padding: '8px',
-    textAlign: 'left'
-  };
-
-
+    const tdStyle = {
+        border: '1px solid #ddd',
+        padding: '8px',
+        textAlign: 'left'
+    };
 
     return (
         <>
-        <div style={{backgroundImage:`url("../../../assets/images/beautiOfEarth.webp")`,
-         backgroundColor:"#FFFFF0",
-    backgroundRepeat:"no-repeat",
-    backgroundSize:"cover",
-    width: '100vw',
-    height: '100vh'
-      
-}} >
+        <div style={{
+            backgroundImage:`url("../../../assets/images/beautiOfEarth.webp")`,
+            backgroundColor:"#FFFFF0",
+            backgroundRepeat:"no-repeat",
+            backgroundSize:"cover",
+            width: '100vw',
+            height: '100vh'
+        }}>
             <div style={{marginLeft:"700px"}}><br/><br/>
-                    <label  style={lableStyle1}><h3>HOTEL DETAILS</h3></label>
-                    </div>
-                    
-                       
-                    
-                
-  <br></br>
-  <div style={cardstyle}>
-                <Table style={tableStyle} className="table table-striped" id="my-table" >
+                <label  style={lableStyle1}><h3>HOTEL DETAILS</h3></label>
+            </div>
+            <br></br>
+            <div style={cardstyle}>
+                <Table style={tableStyle} className="table table-striped" id="my-table">
                     <thead>
                         <tr>
-                           
                             <th style={thStyle}>Hotel Name</th>
                             <th style={thStyle}>Email</th>
                             <th style={thStyle}>Phone Number</th>
@@ -162,7 +145,6 @@ const lableStyle1 = {
                     <tbody>
                         {hotel.map((item) => (
                             <tr key={item._id}>
-                               
                                 <td style={tdStyle}>{item.HotelName}</td>
                                 <td style={tdStyle}>{item.Email}</td>
                                 <td style={tdStyle}>{item.PhoneNum}</td>
@@ -171,10 +153,9 @@ const lableStyle1 = {
                                 <td style={tdStyle}>{item.Description}</td>
                                 <td style={tdStyle}>{item.RoomDetail}</td>
                                 <td style={tdStyle}>{item.PackDetail}</td>
-                                
                                 <td>
                                     <a href="uphotels">
-                                        <button style={buttonEdit } className="btn-success" onClick={() => SetToLocalStorage(
+                                        <button style={buttonEdit} className="btn-success" onClick={() => SetToLocalStorage(
                                             item._id,
                                             item.HotelName,
                                             item.Email,
@@ -187,14 +168,17 @@ const lableStyle1 = {
                                         )}>Edit</button>
                                     </a>
                                 </td>
-
-                                <td><button style={ buttonDelete } className="btn-danger" onClick={() => handleDeleteClick(item._id)}>Delete</button></td>
+                                <td><button style={buttonDelete} className="btn-danger" onClick={() => handleDeleteClick(item._id)}>Delete</button></td>
                             </tr>
                         ))}
                     </tbody>
                 </Table>
                 
-                 <Modal show={showModal} onHide={() => setShowModal(false)}>
+                <Modal
+                    show={showModal}
+                    onHide={() => setShowModal(false)}
+                    centered // This centers the modal vertically
+                >
                     <Modal.Header closeButton>
                         <Modal.Title>Delete Confirmation</Modal.Title>
                     </Modal.Header>
@@ -204,10 +188,9 @@ const lableStyle1 = {
                         <Button variant="danger" onClick={handleDelete}>Delete</Button>
                     </Modal.Footer>
                 </Modal>
-                </div> 
-            </div>
-            
-            <br />
+            </div> 
+        </div>
+        <br />
         </>
     );
 }
