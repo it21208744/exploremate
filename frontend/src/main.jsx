58d0a127f11e86a623fe0d345f0d21ac59304fdc
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-05-01 15:01:36 +0530

    resolved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,16 +6,13 @@ import './index.css'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 
-
 import NiceModal from '@ebay/nice-modal-react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
 
     <ToastContainer position="top-center" />
     <NiceModal.Provider />
   </React.StrictMode>
-
 )
