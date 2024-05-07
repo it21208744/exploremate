e8787b8a41a16f5d29dc94d71bb9d321604d17d4
kasunOA <kasunu@ewisl.net>
2024-05-07 20:11:32 +0530

    new

--- model/user.js
+++ model/user.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' }
+    role: { type: String, enum: ['traveller', 'hotel','taxi'], default: 'traveller' }
     
 });
 
@@ -26,7 +26,7 @@ const validate = (data) => {
         lastName: joi.string().required().label('Last Name'),
         email: joi.string().email().required().label('Email'),
         password: passwordComplexity().required().label('Password'),
-        role: joi.string().valid('user', 'admin').default('user').label('Role')
+        role: joi.string().valid('traveller', 'hotel','taxi').default('traveller').label('Role')
 
     });
     return schema.validate(data);
