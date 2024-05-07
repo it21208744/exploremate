17873de9d257f6bf337653d3f55d4d99f0df798b
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-05-07 22:00:40 +0530

    need to fix booking in unique function 2

--- frontend/src/components/Budgetdisplay.jsx
+++ frontend/src/components/Budgetdisplay.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useActionData } from 'react-router-dom'
 import hotell from '../assets/images/f.jpg'
 // Component to display the AI-generated plan
+import NiceModal from '@ebay/nice-modal-react'
+import BookingHotelInfo from '../components/BookingHotelInfo'
 export default function PlanResult() {
   let data = localStorage.getItem('budgetPlan') // Get the plan data from the action
   data = JSON.parse(data)
@@ -10,38 +12,39 @@ export default function PlanResult() {
     console.log(data[0])
     return <div>No plan data available.</div>
   }
-
+  const viewHotel = (hotel) => {
+    NiceModal.show(BookingHotelInfo, hotel)
+  }
   const plan = data.plan
-  const cardstyle ={
-    overflow : "hidden",
-    boxShadow:"0 2px 20px ",
-    borderRadius: "$radius",
-    transition: "transform 200ms ease-in",
-    padding:"30px",
-    backdropFilter: "blur(5px)",
-    background: "linear-gradient(rgba(255, 255, 255, 0.7),rgba(255, 255, 255, 0.3))",
-    width: "800px",
-    marginLeft:"360px",
-    marginTop: "50px"
-  
+  const cardstyle = {
+    overflow: 'hidden',
+    boxShadow: '0 2px 20px ',
+    borderRadius: '$radius',
+    transition: 'transform 200ms ease-in',
+    padding: '30px',
+    backdropFilter: 'blur(5px)',
+    background:
+      'linear-gradient(rgba(255, 255, 255, 0.7),rgba(255, 255, 255, 0.3))',
+    width: '800px',
+    marginLeft: '360px',
+    marginTop: '50px',
   }
   const buttonStyle = {
-    display: "inline-block",
-    backgroundImage: "linear-gradient(125deg,#042630,#4c7273)",
-    color:"#fff",
-    textTransform:"uppercase",
-    letterSpacing:"2px",
-    fontSize: "16px",
-      width:"130px",
-      height:"36px",
-      border:"none",
-      borderRadius: "5px",
-      cursor: "pointer",
-      
-  };
+    display: 'inline-block',
+    backgroundImage: 'linear-gradient(125deg,#042630,#4c7273)',
+    color: '#fff',
+    textTransform: 'uppercase',
+    letterSpacing: '2px',
+    fontSize: '16px',
+    width: '130px',
+    height: '36px',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  }
   return (
     <>
-    <div
+      <div
         style={{
           background: `url(${hotell})`,
           backgroundRepeat: 'no-repeat',
@@ -50,29 +53,37 @@ export default function PlanResult() {
           height: '180vh',
         }}
       >
-    <div>
-      <div></div>
-      <br></br>
-      {Object.keys(plan).map((key) => {
-        const hotelPlan = plan[key]
-        return (
-          <div style={cardstyle}>
-          <div key={hotelPlan.id}>
-            <h2>{hotelPlan.HotelName}</h2>
-            {/* <p>{hotelPlan.Description}</p> */}
-            <p>Total Expense: {hotelPlan.TotalExpense}</p>
-            <h3>Plan:</h3>
-            <p>Morning: {hotelPlan.Plan.morning}</p>
-            <p>Evening: {hotelPlan.Plan.evening}</p>
-            <p>Night: {hotelPlan.Plan.night}</p>
-            <br />
-            <button type="submit" style={buttonStyle}>Book now</button>
-          </div>
-          </div>
-        )
-      })}
-    </div>
-    </div>
+        <div>
+          <div></div>
+          <br></br>
+          {Object.keys(plan).map((key) => {
+            const hotelPlan = plan[key]
+            return (
+              <div style={cardstyle}>
+                <div key={hotelPlan.id}>
+                  <h2>{hotelPlan.HotelName}</h2>
+                  {/* <p>{hotelPlan.Description}</p> */}
+                  <p>Total Expense: {hotelPlan.TotalExpense}</p>
+                  <h3>Plan:</h3>
+                  <p>Morning: {hotelPlan.Plan.morning}</p>
+                  <p>Evening: {hotelPlan.Plan.evening}</p>
+                  <p>Night: {hotelPlan.Plan.night}</p>
+                  <br />
+                  <button
+                    type="submit"
+                    style={buttonStyle}
+                    onClick={() => {
+                      viewHotel(plan)
+                    }}
+                  >
+                    Book now
+                  </button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </>
   )
 }
